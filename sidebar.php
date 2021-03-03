b65c4f911ad1dfce37f01b81d363b67756edc9c9
Mohamed Sabry <16673178+mohamedsabrymohamed@users.noreply.github.com>
2021-03-03 17:52:55 +0200

    update sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,9 @@
+<?php
+//get user type
+$user_table = new users_table();
+$user_data  = $user_table->retrieve_user_data_by_user_id(get_login_user_id());
+$user_type  = $user_data['user_type'];
+?>
 <div id="wrapper">
     <nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
         <div class="container-fluid d-flex flex-column p-0"><a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
@@ -6,11 +12,16 @@
             </a>
             <hr class="sidebar-divider my-0">
             <ul class="nav navbar-nav text-light" id="accordionSidebar">
-                <li class="nav-item"><a class="nav-link active" href="index.html"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
-                <li class="nav-item"><a class="nav-link" href="profile.html"><i class="fas fa-user"></i><span>Profile</span></a></li>
-                <li class="nav-item"><a class="nav-link" href="table.html"><i class="fas fa-table"></i><span>Table</span></a></li>
-                <li class="nav-item"><a class="nav-link" href="login.html"><i class="far fa-user-circle"></i><span>Login</span></a></li>
-                <li class="nav-item"><a class="nav-link" href="register.html"><i class="fas fa-user-circle"></i><span>Register</span></a></li>
+                <?php if($user_type == 0 ){?>
+                <li class="nav-item"><a class="nav-link active" href="dashboard.php"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="patients.php"><i class="fas fa-table"></i><span>Patients</span></a></li>
+                <li class="nav-item"><a class="nav-link" href="add_new_patient.php"><i class="fas fa-user"></i><span>Add New Patient</span></a></li>
+            <?php }elseif ($user_type == 2 ){?>
+                    <li class="nav-item"><a class="nav-link active" href="dashboard.php"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="icus.php"><i class="fas fa-table"></i><span>ICUs</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="add_icu.php"><i class="fas fa-user"></i><span>Add ICU</span></a></li>
+
+                <?php }?>
             </ul>
             <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
         </div>
