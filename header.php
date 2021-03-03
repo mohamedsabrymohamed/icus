02ce304b6c0c36ad66ac398b814ecbc82f914ee7
Mohamed Sabry <16673178+mohamedsabrymohamed@users.noreply.github.com>
2021-03-03 21:50:19 +0200

    remove search bar

--- header.php
+++ header.php
@@ -9,15 +9,22 @@ if($count_notifications[0]['count_notif'] > 0) {
     $ring_color   = 'green';
     $number_color = 'blue';
 }
+
+$users_table = new users_table();
+$current_user_data = $users_table->retrieve_user_data_by_user_id(get_login_user_id());
+
 ?>
 <nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
     <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
+        <?php if($current_user_data['user_type'] == 1){?>
         <form class="form-inline d-none d-sm-inline-block mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
             <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Search for ...">
                 <div class="input-group-append"><button class="btn btn-primary py-0" type="button"><i class="fas fa-search"></i></button></div>
             </div>
         </form>
+        <?php }?>
         <ul class="nav navbar-nav flex-nowrap ml-auto">
+            <?php if($current_user_data['user_type'] == 1){?>
             <li class="nav-item dropdown d-sm-none no-arrow"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#"><i class="fas fa-search"></i></a>
                 <div class="dropdown-menu dropdown-menu-right p-3 animated--grow-in" aria-labelledby="searchDropdown">
                     <form class="form-inline mr-auto navbar-search w-100">
@@ -27,6 +34,7 @@ if($count_notifications[0]['count_notif'] > 0) {
                     </form>
                 </div>
             </li>
+            <?php }?>
             <li class="nav-item dropdown no-arrow mx-1">
                 <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#"><span class="badge badge-danger badge-counter" style="font-size: 20px;color:<?php echo $number_color;?>;background-color: <?php echo $ring_color;?>;"><?php echo $count_notifications[0]['count_notif'];?></span><i class="fas fa-bell fa-fw" style="font-size: 30px;"></i></a>
                     <div class="dropdown-menu dropdown-menu-right dropdown-list dropdown-menu-right animated--grow-in">
@@ -58,8 +66,6 @@ if($count_notifications[0]['count_notif'] > 0) {
                 <div class="nav-item dropdown no-arrow"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">
                         <span class="d-none d-lg-inline mr-2 text-gray-600 small">
                             <?php
-                            $users_table = new users_table();
-                            $current_user_data = $users_table->retrieve_user_data_by_user_id(get_login_user_id());
                             echo $current_user_data['full_name'];
                             ?></span>
                         <img class="border rounded-circle img-profile" src="assets/img/avatars/avatar1.jpeg"></a>
